b013992cd7dcda6fe3bb8730734f8d7465558aaa
ryo924 <ryo.tominaga24@gmail.com>
2020-11-02 18:59:42 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  validates :sei, presence: true
+  validates :mei, presence: true
+  validates :sei_kana, presence: true
+  validates :mei_kana, presence: true
+  validates :birthday, presence: true
 end
