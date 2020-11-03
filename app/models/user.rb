07f2239cbe49266c2c651ad58452d6dc01dc806e
ryo924 <ryo.tominaga24@gmail.com>
2020-11-03 17:15:22 +0900

    バリデーションの修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   SEI_MEI_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   SEI_MEI_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :encrypted_password, length: {minimum: 6 }
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates :password, length: {minimum: 6 }
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates :sei, presence: true
   validates_format_of :sei, with: SEI_MEI_REGEX, message: 'Full-width characters'
   validates :mei, presence: true
