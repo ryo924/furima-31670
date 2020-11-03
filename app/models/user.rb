896ec33a3b093ca06082f15edc57d75254b34ec8
ryo924 <ryo.tominaga24@gmail.com>
2020-11-03 16:24:20 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  SEI_MEI_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  SEI_MEI_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
   validates :nickname, presence: true
   validates :email, uniqueness: true
+  validates :encrypted_password, length: {minimum: 6 }
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates :sei, presence: true
+  validates_format_of :sei, with: SEI_MEI_REGEX, message: 'Full-width characters'
   validates :mei, presence: true
+  validates_format_of :mei, with: SEI_MEI_REGEX, message: 'Full-width characters'
   validates :sei_kana, presence: true
+  validates_format_of :sei_kana, with: SEI_MEI_KANA_REGEX, message: 'Full-width katakana characters'
   validates :mei_kana, presence: true
+  validates_format_of :mei_kana, with: SEI_MEI_KANA_REGEX, message: 'Full-width katakana characters'
   validates :birthday, presence: true
 end
