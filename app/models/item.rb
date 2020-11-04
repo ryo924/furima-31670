53197e05e10278ce59056d6a8fed525c449823b8
ryo924 <ryo.tominaga24@gmail.com>
2020-11-04 19:10:54 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day
   
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+    validates :image
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :day_id, numericality: { other_than: 1 }
+
 end
