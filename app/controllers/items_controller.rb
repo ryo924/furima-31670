5fbd69d0419c7f3fffae490d34f8121527e7062b
ryo924 <ryo.tominaga24@gmail.com>
2020-11-05 12:16:01 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,26 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    if user_signed_in?
+      @item
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
