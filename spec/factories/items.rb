fb828079e2dc5e846f45b09cce3d17c1d2fa66f9
ryo924 <ryo.tominaga24@gmail.com>
2020-11-05 19:32:59 +0900

    itemモデルのテストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"a"}
+    text {"a"}
+    category_id {2}
+    condition_id {2}
+    shipping_fee_id {2}
+    prefecture_id {2}
+    day_id {2}
+    price {10000}
+    association :user
+
+    after(:build) do |picture|
+      picture.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
