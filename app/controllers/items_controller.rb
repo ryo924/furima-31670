378d578f283bb7f9414858411ccfa5254ac3f735
ryo924 <ryo.tominaga24@gmail.com>
2020-11-06 15:22:07 +0900

    showアクションとルーティングの設定、ビューファイルの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
