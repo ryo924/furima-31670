adc62fbf8361a7e47f151ede000389ec00f0eb02
ryo924 <ryo.tominaga24@gmail.com>
2020-11-07 14:56:13 +0900

    商品情報編集機能の実装（修正後3）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
+  before_action :item_find, only: [:show, :edit]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    item_find
   end
 
   def edit
-    item_find
   end
 
   def update
